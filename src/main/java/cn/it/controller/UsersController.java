61c3feb9a63cc5b19a9e175b4b3f4e57b31b1914
wangze <396415376@qq.com>
2017-03-21 10:14:52 +0800

    补充userscontroller

--- src/main/java/cn/it/controller/UsersController.java
+++ src/main/java/cn/it/controller/UsersController.java
@@ -1,5 +1,31 @@
 package cn.it.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+import cn.it.pojo.Users;
+/**
+	 * @author wz
+	 */
 public class UsersController {
 
-}
+	
+	//添加用户
+		@RequestMapping("addUser")
+		public ModelAndView submitOrder(Users user){
+			return null;
+		}
+	   //删除用户
+		@RequestMapping("deleteUser")
+		public ModelAndView deleteOrder(int userId){
+			return null;
+		}
+		//查询用户
+		@RequestMapping("searchUser")
+		public ModelAndView lookOrderDeatil(int userId){
+			return null;
+			
+		}
+		
+	}
