f223b6a98a7c104314ef97b6d78cf7bdbf8f1a49
zhj <1447199152@qq.com>
2017-03-24 08:49:52 +0800

    zhj <1447199152@qq.com> 张捷 2017.03.24 08:49 内部注释变为乱码，修正

--- src/main/java/cn/it/dao/ItemDao.java
+++ src/main/java/cn/it/dao/ItemDao.java
@@ -26,8 +26,16 @@ public interface ItemDao {
 	 */
 	public void ItemUpdate(int id);
 	/**
-	 * 商品查看；
+	 * 按照商品编号查看商品；
 	 * @param （变量为商品编号 id，类型为int）；
 	 */
-	public void ItemSearch(int id);
+	public void FindItemById(int id);
+	/**
+	 * 按照商品类型查看商品；
+	 * @param typeh
+	 * @param typel
+	 * 
+	 * 在impl中还没有具体方法
+	 */
+	public void FindItemByType(String typeh,String typel);
 }
