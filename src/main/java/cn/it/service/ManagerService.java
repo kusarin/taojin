f6757e1274db3c442596f943ed22dc52a7087968
sunchen <542815501@qq.com>
2017-03-29 11:32:03 +0800

    新增弹窗功能

--- src/main/java/cn/it/service/ManagerService.java
+++ src/main/java/cn/it/service/ManagerService.java
@@ -1,5 +1,7 @@
 package cn.it.service;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import cn.it.pojo.Manager;
--- src/main/java/cn/it/service/impl/ManagerServiceImpl.java
+++ src/main/java/cn/it/service/impl/ManagerServiceImpl.java
@@ -2,6 +2,8 @@ package cn.it.service.impl;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,14 +26,15 @@ public class ManagerServiceImpl implements ManagerService{
 	//管理员登录
 	public ModelAndView login(Manager manager) {
 		ModelAndView mav =new ModelAndView("managerInterface");
-		if(manager==null){
-			if(manager.getAccount()==null||manager.getAccount().equalsIgnoreCase("")||
-					manager.getPassword()==null||manager.getPassword().equalsIgnoreCase("")){
-				mav.setViewName("mlogin");
-			}
+		if(manager.getAccount()==null||manager.getAccount().equalsIgnoreCase("")||
+			manager.getPassword()==null||manager.getPassword().equalsIgnoreCase("")){
+			mav.addObject("error","账户或密码为空！");
+			mav.setViewName("mlogin");
+			
 		}else{
 			Manager m = managerDao.login(manager);
 			if(m == null){
+				mav.addObject("error","账户或密码错误！");
 				mav.setViewName("mlogin");
 			}
 		}
@@ -53,10 +56,13 @@ public class ManagerServiceImpl implements ManagerService{
 			mav.setViewName("addmanager");
 		}else if(manager.getAccount()==null||manager.getAccount().equalsIgnoreCase("")||
 					manager.getPassword()==null||manager.getPassword().equalsIgnoreCase("")){
+			mav.addObject("error","账户名或密码不能为空！");
 			mav.setViewName("addmanager");
 		}else if(flag){
 			managerDao.managerAdd(manager);
 			mav.setViewName("managerInterface");
+		}else{
+			mav.addObject("error","账户名重复！");
 		}
 		return mav;
 	}
