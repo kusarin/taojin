58045565cf8d9c778473dc4c5caac4d20e8371e3
wangze <396415376@qq.com>
2017-04-14 10:38:16 +0800

    密码修改

--- src/main/java/cn/it/controller/UsersController.java
+++ src/main/java/cn/it/controller/UsersController.java
@@ -10,42 +10,53 @@ import org.springframework.web.servlet.ModelAndView;
 
 import cn.it.service.UsersService;
 import cn.it.pojo.Users;
+
 /**
-	 * @author wz
-	 */
+ * @author wz
+ */
 @Controller
 public class UsersController {
-	 @Resource
-	 private UsersService usersService;
+	@Resource
+	private UsersService usersService;
 
 	@RequestMapping("login.do")
-	public ModelAndView login(Users user,HttpSession session) 
-			throws IOException{
-	
-		ModelAndView result = usersService.login(user,session);
+	public ModelAndView login(Users user, HttpSession session)
+			throws IOException {
+
+		ModelAndView result = usersService.login(user, session);
 		return result;
 	}
-		//添加用户
-			@RequestMapping("addUser.do")
-			public ModelAndView addUser(Users user){
-				ModelAndView result = usersService.add(user);
-				return result;
-			}
-	   //删除用户
-		@RequestMapping("deleteUser")
-		public ModelAndView deleteUser(int userId){
-			return null;
-		}
-		//查询用户
-		@RequestMapping("searchUser")
-		public ModelAndView searchUser(int userId){
-			return null;
-			
-		}
-		@RequestMapping("updateUser.do")
-		public ModelAndView updateUser(Users user,HttpSession session){
-			ModelAndView result = usersService.update(user,session);
-			return result;
-			
-		}
+
+	// 添加用户
+	@RequestMapping("addUser.do")
+	public ModelAndView addUser(Users user) {
+		ModelAndView result = usersService.add(user);
+		return result;
 	}
+
+	// 删除用户
+	@RequestMapping("deleteUser")
+	public ModelAndView deleteUser(int userId) {
+		return null;
+	}
+
+	// 查询用户
+	@RequestMapping("searchUser")
+	public ModelAndView searchUser(int userId) {
+		return null;
+	}
+
+	//更新用户信息
+	@RequestMapping("updateUser.do")
+	public ModelAndView updateUser(Users user, HttpSession session) {
+		ModelAndView result = usersService.update(user, session);
+		return result;
+	}
+	
+	//更新用户信息
+		@RequestMapping("changepw.do")
+		public ModelAndView changepw(Users user, HttpSession session) {
+			ModelAndView result = usersService.changepw(user, session);
+			return result;
+		}
+}
